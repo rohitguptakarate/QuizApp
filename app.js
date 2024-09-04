a4f5d93351075250ab3143a536a6016525fc9f9a
rohitguptakarate <rohitguptakarategzp@gmail.com>
2024-09-04 23:31:22 +0530

    changeAgain

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ let clickedpipe = document.getElementById('clickedpipe');
 let yourchoices=document.querySelectorAll(".choice");
 const Maincontainer =document.querySelector(".Maincontainer");
 let DisplayTime =document.getElementById('setTime');
-// console.log(DisplayTime);
 
 
-            // this is use for name geting ..
+
+// this is use for name geting ..
  function btn(){
     let name =document.getElementById('UserName').value;
     document.getElementById('result_name').innerText=name;
     console.log(name);
-    
-    
+       
 }
 
-        // this for every choice is come on by one foreach function ..
+
+ // this for every choice is come on by one foreach function ..
 yourchoices.forEach((choice)=>{
     choice.addEventListener("click", ()=>{
         console.log("clicked calling..")
@@ -25,10 +25,11 @@ yourchoices.forEach((choice)=>{
     })
 })
 
+
 const replce =((userclicked)=>{
     //console.log("i am calling under replace=",userclicked);
     if(userclicked== "clickedpipe"){
-        firstchoice();   // function calling on click btn
+        DisplayfirstChoice();  // function calling on click btn
       
     }
     else if(userclicked=="CSE"){
@@ -36,37 +37,18 @@ const replce =((userclicked)=>{
     }
     else if(userclicked == "Math")
         console.log("yes");
-    
-        thirdchoice();
-})
-
-
-        // this is for first choice 
-const firstchoice =(()=>{
-    window.location.assign("http://127.0.0.1:5500/Quiz.html");
-    // document.querySelector(".Maincontainer").innerHTML="<h1>Pipes and Cisterns</h1>"
-
+      //  thirdchoice();
 })
 
 
 const secondchoice = () =>{
     document.getElementById('hide').style.display='none';
     document.getElementById('demo').style.display="block"
-   
-    
-
-}
-const thirdchoice= () =>{
-   // window.location.assign("http://127.0.0.1:5502/MathQuiz.html");
-
 }
 
 
 
-
-// here quiz start  -------------------------------------------------------------------------------------->
-            /// here to start for----------- CSE---------- Quiz--------------------->
-
+ /// here to start for----------- CSE---------- Quiz--------------------->
 let questionbox = document.body.querySelector("#questionbox");
 let options = document.querySelectorAll("#opt");
 let answer1 = document.querySelector(".answer1");
@@ -84,12 +66,6 @@ let score = 0;    //this is for score
 let AnswerCorrect = 0;    //this is for correct ans
 let AnswerWorng = 0;
 let clickedQuestion =1;   //this is for when userclicked on the option then inc++
-// let timer = 30;
-// let timeinterval;
-// timeinterval = setInterval(updateTimer, 1000);
-
-
-
 
 
 
@@ -197,16 +173,15 @@ const arr = [
   
 ]
 
-
-
-
 let total = arr.length;
 let index = 0;
+
 const loadQuestion = () => {
     displayHeading();
     if(index==total){
         return endQuiz();
     }
+
     let data = arr[index];
     // console.log(data);
     questionbox.innerText = `${index + 1}.  ${data.question}`;
@@ -214,34 +189,24 @@ const loadQuestion = () => {
     answer2.innerText = data.b;
     answer3.innerText = data.c;
     answer4.innerText = data.d;
-    
-
 }
-loadQuestion();  // automatic question load when click ---------------->
+loadQuestion();  // automatic question load when click 
+
 
 
 // next button clicked 
 function quizbutton() {
     // ---->
     index++;              // this is incriment again and again..
-    console.log(index);
     loadQuestion();      // here calling again and again when clicked to the btn..
 
 }
 
-
 // Geting answer -->
-
 options.forEach((opt, inde) => {
-    
     opt.addEventListener("click", () => {
-        console.log("checked");
         document.getElementById("attemptQuestion").innerText=clickedQuestion++;  //this is used for print on the Answer Sheet ----------------->
         let data = arr[index];
-        //console.log(inde);
-        //console.log(data.correct);
-
-
         if (inde == data.correct) {
             score++;    // score inc..
             AnswerCorrect++;
@@ -256,48 +221,42 @@ options.forEach((opt, inde) => {
         quizbutton();
     });
    
-
 });
 
-   
-
-// this is for quize end and then result show  --------------------------------------------------->
-
+// this is for quize end and then result show 
 const endQuiz= ()=>{
-   
+  
+    document.getElementById('quiz1').style.display='none'; 
     document.getElementById('demo').style.display='none';
-   document.getElementById('showresult').style.display="block"
-    
-     document.getElementById('total_questions').innerText=total;
-   
-     document.getElementById('CorrectAns').innerText=AnswerCorrect ;
+
+    document.getElementById('showresult').style.display="block"
+  //Quiz1
+    document.querySelector('.Quiz1Ans').innerText=AnswerCorrect;
+    document.querySelector('.Quiz1Worng').innerText=AnswerWorng;
+    document.getElementById('total_questions').innerText=AllQue;
+  //Quiz2
+    document.getElementById('total_questions').innerText=total;
+    document.getElementById('CorrectAns').innerText=AnswerCorrect ;
     document.getElementById('worngAnss').innerText=AnswerWorng;
-   
-
-
 }
 
 function displayHeading(){
-    document.getElementById('HideHeading').innerText="";
-    
-    
+    document.getElementById('HideHeading').innerText="";   
 }
 
 
 
 
-// Go to Home Page  Function---------------------->
+// Go to Home Page  Function
 function GoHome(){
      document.getElementById('hide').style.display="block"
      document.getElementById('showresult').style.display="none";
-
     console.log("clicked")
 }
 
 
-// calculate  ------------------>
+// calculate
 const calculate =()=>{
-   
    let calcToatal =Math.floor(AnswerCorrect/10 * 100);
    console.log("total =",calcToatal);
    document.getElementById('Percenrage').innerText=calcToatal;
@@ -306,13 +265,173 @@ const calculate =()=>{
 }
 
 
-// function updateTimer() {
-//     if (timer > 0) {
-//         timer--;
-//         DisplayTime.textContent =  timer + 's';
-//         console.log('timeinterval', timeinterval); 
-//     }
-// }
+
+//............................................Quiz1 Here Start..................................
+
+function DisplayfirstChoice(){
+    document.getElementById('hide').style.display='none';
+    document.getElementById('quiz1').style.display='block';
+}
+
+let que1 = document.querySelector(".que1");
+let ans1 = document.querySelector('.ans1');
+let ans2 = document.querySelector('.ans2');
+let ans3 = document.querySelector('.ans3');
+let ans4 = document.querySelector('.ans4');
+let Quiz1Opt = document.querySelectorAll('#quiz1Opt');
+
+
+
+// Quiz1 All Questions
+const Quiz1Question = [
+    {
+        'question': "Pipe A can fill a tank in 4 hours, while Pipe B can fill the same tank in 6 hours. If both pipes are opened simultaneously, how long will they take to fill the tank together?",
+        a: "2.4 hours",
+        b: "2.5 hours",
+        c: "2.6 hours",
+        d: "2.7 hours",
+        correct: "0",
+
+    },
+    {
+        'question': "Pipe X can fill a tank in 8 hours, and Pipe Y can fill the same tank in 12 hours. If Pipe X is opened for 2 hours and then Pipe Y is opened, how long will it take to fill the tank completely?",
+        a: "3.4 hours",
+        b: "4 hours",
+        c: "6 hours",
+        d: "2 hours",
+        'correct': "1",
+
+    },
+    {
+        'question': "A cistern can be filled by two pipes, A and B, in 10 hours and 15 hours respectively. If both pipes are open together, how long will it take to fill the cistern?",
+        a: "5.5 hours",
+        b: "6 hours",
+        c: "6.5 hours",
+        d: "7 hours",
+        'correct': "1",
+
+    },
+    {
+        'question': "A cistern can be filled by three pipes, A, B, and C, in 6 hours, 8 hours, and 12 hours respectively. If all three pipes are open together, how long will it take to fill the cistern?",
+
+        a: "3 hours",
+        b: "4.5 hours",
+        c: "4 hours",
+        d: "5 hours",
+        'correct': "2",
+
+    },
+    {
+        'question': "A cistern is filled by two pipes, X and Y. Pipe X alone can fill the cistern in 20 hours and pipe Y alone can fill it in 30 hours. If both pipes are open together, how long will it take to fill the cistern?",
+    
+        a: "10 hours",
+        b: "12 hours",
+        c: "15 hours",
+        d: "18 hours",
+        'correct': "0",
+
+    },
+    {
+        'question': "A cistern can be emptied by pipe M in 8 hours and by pipe N in 12 hours. If both pipes are open together, how long will it take to empty the cistern?",
+      
+        a: "4.8 hours",
+        b: "5.5 hours",
+        c: "6 hours",
+        d: "6.5 hours",
+        'correct': "0",
+
+    },
+    {
+        'question': "If pipe A can fill a cistern in 8 hours and pipe B can fill it in 12 hours, and if pipe B is twice as fast as pipe A, how long will it take for the cistern to be filled if both pipes are working together?",
+      
+        a: "4 hours",
+        b: "4.8 hours",
+        c: "6 hours",
+        d: "5 hours",
+        'correct': "1",
+
+    },
+    {
+        'question': "A cistern has two pipes, A and B. Pipe A can fill the cistern in 15 hours and pipe B can empty it in 20 hours. If both pipes are open together, how long will it take to fill the cistern?",
+      
+        a: "10 hours",
+        b: "12 hours",
+        c: "15 hours",
+        d: "18 hours",
+        'correct': "3",
+
+    },
+    {
+        'question': "A cistern is filled by three pipes, A, B, and C. Pipe A can fill the cistern in 4 hours, pipe B in 6 hours, and pipe C in 8 hours. If all three pipes are open together for 2 hours, how much of the cistern is filled?",
+
+      
+        a: "1/4",
+        b: "1/3",
+        c: "1/2",
+        d: "2/3",
+        'correct': "1",
+
+    },
+    {
+        'question': "A cistern can be filled by two pipes, A and B, in 12 hours and 18 hours respectively. If pipe A is open for 4 hours and then pipe B is opened, how long will it take for pipe B alone to fill the remaining part of the cistern?",
+  
+        a: " 6 hours",
+        b: " 8 hours",
+        c: " 10 hours",
+        d: " 12 hours",
+        'correct': "0",
+
+    },
+  
+]
+let AllQue = Quiz1Question.length;
+
+
+let inx =0;
+const loadQuiz1 = () => {
+    displayHeading();
+    if(inx==AllQue){
+        return endQuiz();    // got to end Quiz
+    }
+    let data1 = Quiz1Question[inx];
+     console.log(data1);
+      que1.innerText = `${inx + 1}.  ${data1.question}`;
+      ans1.innerText = data1.a;
+      ans2.innerText = data1.b;
+      ans3.innerText = data1.c;
+      ans4.innerText = data1.d;
+
+}
+loadQuiz1();
+
+
+//clicked Options
+Quiz1Opt.forEach((opt , indx) => {
+    opt.addEventListener("click", () =>{
+        let data1 = Quiz1Question[inx];
+        if(indx == data1.correct){
+            AnswerCorrect++;
+            console.log("Right");
+        }
+        else{
+            AnswerWorng++;
+            console.log("Worng");
+        }
+
+       quizbtn(); // Call when clicked options. 
+
+    })
+
+})
+
+
+// next button clicked 
+function quizbtn() {
+    inx++;              // this is incriment again and again..
+    console.log(index);
+    loadQuiz1();      // here calling again and again when clicked to the btn..
+
+}
 
 
 
